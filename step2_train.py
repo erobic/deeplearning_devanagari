aeb8f96ab67ce32b8ee4ed8ae7443b723daa35f9
robikshrestha <robikshrestha@gmail.com>
2016-10-16 12:22:16 +0545

    Added a ResNet

--- step2_train.py
+++ step2_train.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 TFRECORDS_TRAIN_DIR = os.path.join(proj_constants.DATA_DIR, 'tfrecords', 'train')
 TFRECORDS_TEST_DIR = os.path.join(proj_constants.DATA_DIR, 'tfrecords', 'test')
-BATCH_SIZE = 120
+BATCH_SIZE = 128
 EPOCHS = 100
 LEARNING_RATE = 8e-4
 SUMMARIES_DIR = os.path.join(proj_constants.DATA_DIR, 'summary')
@@ -70,12 +70,16 @@ def bias_variable(shape, name):
     return tf.Variable(initial)
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, strides=[1, 1, 1, 1]):
+    return tf.nn.conv2d(x, W, strides=strides, padding='SAME')
 
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def avg_pool_2x2(x, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1]):
+    return tf.nn.avg_pool(x, ksize=ksize, strides=strides, padding='VALID')
 
 
 def local_response_normaliztion(x):
@@ -210,12 +214,168 @@ def show_all():
     show_image(images, labels_eval)
 
 
+def build_ResNet():
+    """Builds a ResNet to train the model
+
+    Returns:
+        x: placeholder for input images
+        y_: placeholder for actual labels
+        keep_prob: dropout parameter (1 = no dropout)
+        train_step: Optimizer to train the model
+        accuracy: op to calculate accuracy of data
+    """
+    image_reduction_factor = 1
+    # Input images and labels
+    with tf.name_scope("input"):
+        x = tf.placeholder(tf.float32, shape=[None, proj_constants.WIDTH * proj_constants.HEIGHT])
+        y_ = tf.placeholder(tf.int32, shape=[None, proj_constants.CLASSES])
+        keep_prob = tf.placeholder(tf.float32)
+        x_image = tf.reshape(x, [-1, proj_constants.WIDTH, proj_constants.HEIGHT, 1])
+
+    with tf.name_scope("preprocess"):
+        with tf.name_scope("preprocess_1"):
+            p1_maps = 16
+            W_p1 = weight_variable([3, 3, 1, p1_maps], name="weight_p1")
+            b_p1 = bias_variable([p1_maps], name="bias_p1")
+            conv_p1 = conv2d(x_image, W_p1) + b_p1
+            relu_p1 = tf.nn.relu(conv_p1)
+            norm_p1 = local_response_normaliztion(relu_p1)
+            output_p1 = norm_p1
+            print("relu_p1")
+            print(relu_p1)
+        with tf.name_scope("preprocess_2"):
+            p2_maps = 16
+            W_p2 = weight_variable([3, 3, p1_maps, p2_maps], name="weight_p2")
+            b_p2 = bias_variable([p2_maps], name="bias_p2")
+            conv_p2 = conv2d(output_p1, W_p2) + b_p2
+            relu_p2 = tf.nn.relu(conv_p2)
+            norm_p2 = local_response_normaliztion(relu_p2)
+            output_p2 = norm_p2
+            print("relu_p2")
+            print(relu_p2)
+        with tf.name_scope("preprocess_3"):
+            # Use convolutional layer with stride = 2 instead of max pooling layer
+            p3_maps = 32
+            W_p3 = weight_variable([3, 3, p2_maps, p3_maps], name="weight_p3")
+            b_p3 = bias_variable([p3_maps], name="bias_p3")
+            conv_p3 = conv2d(output_p2, W_p3, strides=[1, 2, 2, 1]) + b_p3
+            relu_p3 = tf.nn.relu(conv_p3)
+            norm_p3 = local_response_normaliztion(relu_p3)
+            output_p3 = norm_p3
+            image_reduction_factor *= 2
+            print("relu_p3")
+            print(relu_p3)
+            # relu_p3 = tf.nn.relu(norm_p3)
+
+    with tf.name_scope("res_net_1"):
+        with tf.name_scope("res_net_1_conv_1"):
+            r1c1_maps = 64
+            W_r1c1 = weight_variable([3, 3, p3_maps, r1c1_maps], name="weight_r1c1")
+            b_r1c1 = bias_variable([r1c1_maps], name="bias_r1c1")
+            conv_r1c1 = conv2d(output_p3, W_r1c1) + b_r1c1
+            relu_r1c1 = tf.nn.relu(conv_r1c1)
+            #norm_r1c1 = local_response_normaliztion(relu_r1c1)
+            output_r1c1 = relu_r1c1
+            print("relu_r1c1")
+            print(relu_r1c1)
+        with tf.name_scope("res_net_1_conv_2"):
+            r1c2_maps = 32
+            W_r1c2 = weight_variable([3, 3, r1c1_maps, r1c2_maps], name="weight_r1c2")
+            b_r1c2 = bias_variable([r1c2_maps], name="bias_r1c2")
+            conv_r1c2 = conv2d(output_r1c1, W_r1c2) + b_r1c2
+            #norm_r1c2 = local_response_normaliztion(conv_r1c2)
+            output_r1c2 = conv_r1c2
+            print("output_r1c2")
+            print(output_r1c2)
+        with tf.name_scope("res_net_1_sum"):
+            sum_r1 = output_p3 + output_r1c2
+            relu_sum_r1 = tf.nn.relu(sum_r1)
+            output_r1 = relu_sum_r1
+            print("relu_sum_r1")
+            print(relu_sum_r1)
+        with tf.name_scope("res_net_1_conv_3"):
+            r1c3_maps = 64
+            W_r1c3 = weight_variable([3, 3, r1c2_maps, r1c3_maps], name="weight_r1c3")
+            b_r1c3 = bias_variable([r1c3_maps], name="bias_r1c3")
+            conv_r1c3 = conv2d(output_r1, W_r1c3, strides=[1, 2, 2, 1]) + b_r1c3
+            relu_r1c3 = tf.nn.relu(conv_r1c3)
+            norm_r1c3 = local_response_normaliztion(relu_r1c3)
+            output_r1c3 = norm_r1c3
+            image_reduction_factor *= 2
+            print("relu_r1c3")
+            print(relu_r1c3)
+
+    with tf.name_scope("last_layer"):
+        with tf.name_scope("last_conv_1"):
+            lc1_maps = proj_constants.CLASSES
+            W_lc1 = weight_variable([3, 3, r1c3_maps, lc1_maps], name="weight_lc1")
+            b_lc1 = bias_variable([lc1_maps], name="bias_lc1")
+            conv_lc1 = conv2d(output_r1c3, W_lc1) + b_lc1
+            relu_lc1 = tf.nn.relu(conv_lc1)
+            norm_lc1 = local_response_normaliztion(conv_lc1)
+            output_lc1 = norm_lc1
+            print("relu_lc1")
+            print(relu_lc1)
+        with tf.name_scope("flatten1"):
+            flatten1_size = int(proj_constants.WIDTH * proj_constants.HEIGHT * lc1_maps/
+                                                       (image_reduction_factor * image_reduction_factor))
+            flatten1 = tf.reshape(output_lc1, [-1, flatten1_size])
+            print("flatten1")
+            print(flatten1)
+        with tf.name_scope("fully_connected1"):
+            fc1_size = 1024
+            W_fc1 = weight_variable([flatten1_size, fc1_size], name="weight_fc1")
+            b_fc1 = bias_variable([fc1_size], name="bias_fc1")
+            linear_fc1 = tf.matmul(flatten1, W_fc1) + b_fc1
+            relu_fc1 = tf.nn.relu(linear_fc1)
+            drop_fc1 = tf.nn.dropout(relu_fc1, keep_prob)
+            output_fc1 = drop_fc1
+
+        # with tf.name_scope("avg_pool"):
+        #     avg_pool = avg_pool_2x2(output_lc1, ksize=[1, int(proj_constants.WIDTH/image_reduction_factor),
+        #                                          int(proj_constants.HEIGHT/image_reduction_factor), 1])
+        #     print("avg_pool")
+        #     print(avg_pool)
+        # with tf.name_scope("flatten"):
+        #     flatten = tf.reshape(avg_pool, [-1, proj_constants.CLASSES])
+        #     print("flatten")
+        #     print(flatten)
+        with tf.name_scope("fully_connected2"):
+            fc2_size = proj_constants.CLASSES
+            W_fc2 = weight_variable([fc1_size, fc2_size], name="weight_fc2")
+            b_fc2 = bias_variable([fc2_size], name="bias_fc2")
+            linear_fc2 = tf.matmul(output_fc1, W_fc2) + b_fc2
+            output_fc2 = linear_fc2
+        with tf.name_scope("soft_max"):
+            soft_max = tf.nn.softmax(output_fc2)
+            prediction = soft_max
+            print("prediction")
+            print(prediction)
+
+    with tf.name_scope("cross_entropy"):
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(tf.cast(y_, tf.float32) * tf.log(prediction), reduction_indices=[1]))
+        tf.scalar_summary("Cross Entropy", cross_entropy)
+
+    with tf.name_scope("train"):
+        train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+    with tf.name_scope("correct_prediction"):
+        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_, 1))
+
+    with tf.name_scope("accuracy"):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary("accuracy", accuracy)
+
+    return x, y_, keep_prob, train_step, accuracy
+
+
 def train_CNN():
     highest_accuracy = 0
     """Trains CNN. Prints out accuracy every 100 steps. Prints out test accuracy at the end."""
     with tf.Graph().as_default():
         # Build the CNN
-        x, y_, keep_prob, train_step, accuracy = build_CNN()
+        #x, y_, keep_prob, train_step, accuracy = build_CNN()
+        x, y_, keep_prob, train_step, accuracy = build_ResNet()
 
         # Create and initialize the ops
         images, labels = read_batches(TFRECORDS_TRAIN_DIR, batch_size=BATCH_SIZE)
@@ -251,7 +411,7 @@ def train_CNN():
                     # Evaluate train accuracy every 10th step
                     summary, train_accuracy = sess.run([merge_summary, accuracy], feed_dict={x: images_eval, y_: label_vectors, keep_prob: 1.0})
                     train_writer.add_summary(summary, step_num)
-                    print("Step: %d Training accuracy: %g" %(step_num, train_accuracy))
+                    print("Step: %d Training accuracy: %g" % (step_num, train_accuracy))
 
                 if step_num % 500 == 0:
                     # Evaluate test accuracy every 100th step
@@ -271,7 +431,8 @@ def train_CNN():
             print("Total Steps = %d" % step_num)
         finally:
             coord.request_stop()
-
+            train_writer.flush()
+            test_writer.flush()
         coord.join(threads)
         sess.close()
 
